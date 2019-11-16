f6d7c26c5f04d877134f3c5050b8bc5f730cfdc9
Milena <milenavi@kth.se>
2019-11-16 14:11:50 +0100

    window.innerWidth part of the code is added.

--- src/view/sidebarView.js
+++ src/view/sidebarView.js
@@ -7,34 +7,12 @@ class SidebarView
 
     render() {
         this.container.append(
-            /*makeWithAttr("div", "sideBarView", "item-sidebar", [
-                  makeWithAttr("div", "", "stretchBottom", [
-                makeWithAttr("h3", "", "my-dinner-text", "My Dinner"),
-                makeWithAttr("p", "", "", "People"),
-                makeInput("inputNumOfPeople", "number", "0", "1", "0", ""),
-                make("br", ""),
-                makeWithAttr("div", "divBorder","space", ["DishName ", " Cost"]),
-                makeWithAttr("div", "divBorderNothing", "space", ["Lasagne ", "SEK"]),
-                make("br", ""),
+           makeWithAttr("div", "sidebarContainer", "", [
+               makeWithAttr("div", "collapsibleSidebar", "", [
 
-                makeWithAttr("div", "", "flex-content-Btn", [
-                makeWithAttr("button", "flexBtn", "sidebarBtn", "Confirm Dinner") ]) ])
-
-           ])*/
-            /*makeWithAttr("div", "", "", [
-                makeWithAttr("div", "mySidenav", "sidenav", [
-                    makeHyperlink("javascript:void(0)", "", "closebtn", "closeNav()", "&times")
-                ]),
-
-                makeWithAttr("p", "", "", "Click on the sidebar button!"),
-                makeSpan("", "spantext", "closeNav()", "&#9776; open")
-
-            ])*/
-
-           makeWithAttr("div", "divBorder", "", [
-                makeWithAttr("button", "", "collapsible", "Collapsing Sidebar"),
-                makeWithAttr("div", "", "content", [
-                    makeWithAttr("p", "", "", [
+               // makeWithAttr("button", "", "collapsible", "Collapsing Sidebar"),
+               // makeWithAttr("div", "", "content", [
+                   // makeWithAttr("p", "", "", [
                         makeWithAttr("div", "sideBarView", "item-sidebar", [
                             makeWithAttr("div", "", "stretchBottom", [
                                 makeWithAttr("h3", "", "my-dinner-text", "My Dinner"),
@@ -48,29 +26,54 @@ class SidebarView
                                 makeWithAttr("div", "", "flex-content-Btn", [
                                     makeWithAttr("button", "flexBtn", "sidebarBtn", "Confirm Dinner") ]) ])
 
-                        ])
-                    ])
-                ]),
+                        ])//,
+                    //])
+                //]),
 
-           makeWithAttr("script", "", "", "var search = document.getElementById(\"searchContainer\"); " +
-               "var coll = document.getElementsByClassName(\"collapsible\");\n" +
-               "                var i;\n" +
-               "\n" +
-               "                for (i = 0; i < coll.length; i++) {\n" +
-               "                    coll[i].addEventListener(\"click\", function() {\n" +
-               "                        this.classList.toggle(\"active\");\n" +
-               "                        var content = this.nextElementSibling;\n" +
-               "                        if (search.style.display === \"none\" && content.style.display === \"block\") {\n" +
-               "                            search.style.display = \"flex\";\n" +
-               "                            content.style.display = \"none\";\n" +
-               "                        } else {\n" +
-               "                            search.style.display = \"none\";\n" +
-               "                            content.style.display = \"block\";\n" +
-               "                        }\n" +
-               "                    });\n" +
-               "                }")
+                       /*makeWithAttr("script", "", "", "var coll = document.getElementsByClassName(\"collapsible\");\n" +
+                           "                var i;\n" +
+                           "\n" +
+                           "                for (i = 0; i < coll.length; i++) {\n" +
+                           "                    coll[i].addEventListener(\"click\", function() {\n" +
+                           "                        this.classList.toggle(\"active\");\n" +
+                           "                        var content = this.nextElementSibling;\n" +
+                           "                        if (content.style.display === \"block\") {\n" +
+                           "                            content.style.display = \"none\";\n" +
+                           "                        } else {\n" +
+                           "                            content.style.display = \"block\";\n" +
+                           "                        }\n" +
+                           "                    });\n" +
+                           "                }" +
+                           "")*/
 
                ])
+
+                /*makeWithAttr("script", "", "", "var w = window.innerWidth;\n" +
+                    "var ww = (window.innerWidth >= 450);\n" +
+                    "\n" +
+                    "if(ww === true) {" +
+                    "document.getElementById(\"searchContainer\").innerHTML;" +
+                    "} else {" +
+                    "document.getElementById(\"searchContainer\").innerHTML = \"Width: \" + w; " +
+                    "}" +
+                    "")*/
+            ]),
+
+               makeWithAttr("script", "", "", "function displayWindowSize(){\n" +
+                   "        var w = window.innerWidth;\n" +
+                   "        \n" +
+                   "        if(w <= 700) {\n" +
+                   "        document.getElementById(\"collapsibleSidebar\").innerHTML = \"Smaller: \" + w; " +
+                   "        }\n" +
+                   "        else {\n" +
+                   "        document.getElementById(\"collapsibleSidebar\").innerHTML = \"Larger: \" + w; " +
+                   "        }\n" +
+                   "    }\n" +
+                   "     \n" +
+                   "    window.addEventListener(\"resize\", displayWindowSize);\n" +
+                   "    \n" +
+                   "    displayWindowSize();")
+
         );
 
 
